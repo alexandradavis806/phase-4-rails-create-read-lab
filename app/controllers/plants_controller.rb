21295911400e7c3d9e6e7d1d8e214af56fe69650
Alexandra Davis <alex.davis806@gmail.com>
2022-05-17 08:06:00 -0700

    commit tree e871fa44e707c284ded2ba73bfe6515c1f373b5d

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,22 @@
+class PlantsController < ApplicationController
+
+    def index
+        plants = Plant.all
+        render json: plants, status: :ok
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        # render json: plant, status: :ok
+        if plant 
+            render json: plant, status: :ok
+        else 
+            render json: { error: "plant not found " }, status: :not_found
+        end 
+    end 
+
+    def create
+        plant = Plant.create(params.permit(:name, :image, :price))
+        render json: plant, status: :created
+    end 
+end
